36132eda3b48e69769f8d8ac732aa12d5d3d4596
HusseinEC <hussein.kasem200@gmail.com>
2024-03-19 15:29:00 +0100

    Add project files.

---
+++ WebApp/Pages/Sections/_FooterPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Shared
+{
+    public class _FooterPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
